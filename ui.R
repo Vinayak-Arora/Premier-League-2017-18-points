476f1d67f699dcb100e69b593b54fcd44e48d187
Vinayak-Arora <43749406+Vinayak-Arora@users.noreply.github.com>
2021-01-26 14:04:15 +0100

    Update and rename app.R to ui.R

---
+++ ui.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
+
+#save_in <- "C://Program Files//RStudio//"
+#file_name <- "points.csv"
+#pts <- read.csv(paste(save_in, file_name,sep = ""))
+pts <- read.csv("points.csv")
+
+#file_name <- "premclean.csv"
+#matchData <- read.csv(paste(save_in, file_name,sep = ""))
+matchData <- read.csv("premClean.csv")
+
+teams <- as.factor(pts$Team) %>%
+          unique() %>%
+            as.vector()
+
+ui <- fluidPage(titlePanel("Premier League 2017/18 Matchday Record"),
+                sidebarLayout(
+                  sidebarPanel(
+                    fluidRow(
+                      #Names of all 20 teams to be selected from
+                      #By default, all teams are selected
+                      selectizeInput("selectTeams", "Choose Teams :", teams, selected = teams, multiple = TRUE, options = NULL)
+                    )
+                  ),
+                  mainPanel(
+                    fluidRow(
+                      column(width = 10, plotlyOutput("coolplot", height = 750))
+                    ) ,
+                    fluidRow(
+                      #A section that displays match info whenever a point on the graph is clicked on
+                      verbatimTextOutput("matchInfo")
+                    )
+                  )
+                )
+              )
+

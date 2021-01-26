85ead49ed0484582ae6475e3863874cfffc5a0b8
Vinayak-Arora <43749406+Vinayak-Arora@users.noreply.github.com>
2021-01-26 15:32:55 +0100

    Create init.R

---
+++ init.R
@@ -0,0 +1,9 @@
+my_packages = c("dplyr", "ggplot2", "plotly")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
